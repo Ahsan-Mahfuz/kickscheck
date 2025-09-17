c260ee4de0f1f92151ee7c577a5d8158af982a11
Ahsan-Mahfuz <ahsanmahfuz0@gmail.com>
2025-09-17 14:25:06 +0600

    update the design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,38 @@
-import LayoutWrapper from '@/components/layoutWrapper/LayoutWrapper'
-import './globals.css'
-import { Geist, Geist_Mono, Poppins } from 'next/font/google'
-import { Toaster } from 'react-hot-toast'
+import LayoutWrapper from "@/components/layoutWrapper/LayoutWrapper";
+import "./globals.css";
+import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Toaster } from "react-hot-toast";
 
 const poppins = Poppins({
-  variable: '--font-poppins',
-  subsets: ['latin'],
-  weight: ['400', '500', '600', '700'],
-  display: 'swap',
-})
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+  display: "swap",
+});
 
 const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
 const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
-  title: 'KickCheck',
+  title: "KickCheck",
   description:
-    'KickCheck is a platform for people to authenticate sneakers, view market trends and make informed purchasing decisions.',
-}
+    "KickCheck is a platform for people to authenticate sneakers, view market trends and make informed purchasing decisions.",
+  icons: {
+    icon: "/logo.svg",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -43,13 +46,13 @@ export default function RootLayout({
           toastOptions={{
             style: {
               backgroundColor: `rgba(25, 48, 80, 1)`,
-              color: 'white',
-              borderRadius: '0px',
-              border: '1px solid black',
+              color: "white",
+              borderRadius: "0px",
+              border: "1px solid black",
             },
           }}
         />
       </body>
     </html>
-  )
+  );
 }
